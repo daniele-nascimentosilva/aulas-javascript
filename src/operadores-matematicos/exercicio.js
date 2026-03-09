e1efc90a1d554ef5e7347bd188e353f9ab3bcc5d
Daniele Silva <daniele.nsilva@icloud.com>
2026-03-09 12:37:05 -0300

    adicionado arquivos da aula operadores-matematicos

---
+++ src/operadores-matematicos/exercicio.js
@@ -0,0 +1,24 @@
+const numero1 = 2
+const numero2 = 3
+const soma = numero1 + numero2
+console.log(soma)
+
+const numero3 = 20
+ const numero4 = 3
+ const subtracao = numero3 - numero4
+ console.log(subtracao)
+
+ const numero5 = 20
+ const numero6 = 3
+ const multiplicacao = numero5 * numero6
+ console.log(multiplicacao)
+
+    const numero7 = 20
+    const numero8 = 3
+    const divisao = numero7 / numero8
+    console.log(divisao)
+
+    const numero9 = 20
+    const numero10 = 3
+    const resto = numero9 % numero10
+    console.log(resto)
---
+++ src/operadores-matematicos/index.js
@@ -0,0 +1,35 @@
+// operadores matemáticos, usado para realizar cálculos básicos
+
+//soma
+
+// const numero1 = 2
+// const numero2 = 3
+// const soma = numero1 + numero2
+// console.log(soma)
+
+//subtração
+// const numero1 = 20
+//  const numero2 = "3"
+//  const subtracao = numero1 - numero2
+//  console.log(subtracao)
+
+//multiplicação
+// const numero1 = 20
+//  const numero2 = "3"
+//  const multiplicacao = numero1 * numero2
+//  console.log(multiplicacao)
+
+//divisão
+// const numero1 = 20
+//  const numero2 = "3"
+//  const divisao = numero1 / numero2
+//  console.log(divisao)
+
+ //resto da divisão - %
+//  const numero1 = 20
+//  const numero2 = 3
+//  const resto = numero1 % numero2
+//  console.log(resto)
+
+console.log(10 % 2 === 0) // true - significa que o número é par
+console.log(11 % 2 === 0) // false - significa que o número é ímpar
