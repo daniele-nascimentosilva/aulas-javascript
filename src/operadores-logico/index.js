a31e3b82aed92e96ddea93a72cbeecbbd7bd8070
Daniele Silva <daniele.nsilva@icloud.com>
2026-03-09 12:32:18 -0300

    adicionado arquivo da aula operadores-logico

---
+++ src/operadores-logico/index.js
@@ -0,0 +1,23 @@
+// == operador de iguel, leva em consideração apenas o valor da variável e não o tipo dela.
+//const numero1 = 3
+//const numero2 = '3'
+
+//console.log(numero1 == numero2) // true
+
+// === operador de idêntico, leva em consideração tanto o tipo quanto o valor 
+//const numero1 = 3
+//const numero2 = '3'
+//console.log(numero1 === numero2) // false
+
+// !== operador não idêntico, leva em consideração tanto o tipo quanto o valor
+const numero1 = 3
+const numero2 = '3'
+console.log(numero1 !== numero2)
+
+// != operador de diferente, leva em consideração apenas o valor da variável e não o tipo dela.
+
+// // menor que  e menor ou igual que
+// /const numero1 = 3
+// const numero2 = 5
+// console.log/(numero1 < numero2) // true
+// console.log(numero1 <= numero2) // true
