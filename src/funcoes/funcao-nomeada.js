99a4ca5d2dc7008d884d740b54510e592f6461ab
Daniele Silva <daniele.nsilva@icloud.com>
2026-03-09 12:22:11 -0300

    adicionado arquivo da aula funções

---
+++ src/funcoes/funcao-nomeada.js
@@ -0,0 +1,12 @@
+// funções nomeadas
+// function saudarUsuario(nome) {
+//     console.log(`Seja bem vinda, ${nome}`);
+// }
+
+// saudarUsuario("Daniele")
+
+function somar(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+console.log(somar(5, 7));
